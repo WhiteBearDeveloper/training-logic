977ca27f36394e4aaf3ab00a0aed278e10da1469
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2023-02-10 23:14:48 +0700

    fix: common - remove common package

--- src/common/types.ts
+++
@@ -1,7 +0,0 @@
-import { CommonWithId } from "@whitebeardeveloper/common-logic/logic/types/base.types";
-
-export interface WithProfileId {
-  profileId: number;
-}
-
-export interface CommonWithProfileId extends CommonWithId, WithProfileId {}
